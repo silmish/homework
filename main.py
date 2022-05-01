34ec5280b680537956b863b37a28e1f8aaefc74d
silmis <pate.henriksson@gmail.com>
2022-05-01 11:15:48 +0300

    Minor fixes and document update

--- main.py
+++ main.py
@@ -23,23 +23,30 @@ db_products: List[Product] = [
     ),
     Product(
         id=999,
-        name="75\" OLED TV",
+        name='75" OLED TV',
         price="1333.37"
     )
 ]
+
 db_orders:  List[Orders] = []
 
+# Get list of all products
+
 @app.get("/api/products")
-async def fetch_products():
+async def get_products():
     return db_products
+
+# Create new order
 
 @app.post("/api/orders")
 async def create_order(order: Orders):
     db_orders.append(order)
     return {"id": order.id}
 
+# Get order information    
+
 @app.get("/api/orders/{order_id}")
-async def fetch_orders(order_id: UUID):
+async def get_orders(order_id: UUID):
     for order in db_orders:
         if order.id == order_id:
             return order
@@ -47,6 +54,8 @@ async def fetch_orders(order_id: UUID):
         status_code = 404,
         detail=f"Order with id: {order_id} does not exist"
     )
+
+# Get order porduct information
 
 @app.get("/api/orders/{order_id}/products")
 async def get_order_products(order_id: UUID):
@@ -57,6 +66,8 @@ async def get_order_products(order_id: UUID):
         status_code = 404,
         detail=f"Order with id: {order_id} does not exist"
     )
+
+# Add new product to the order    
 
 @app.post("/api/orders/{order_id}/products", response_model=Order_products)
 async def add_product_order(order_id: UUID, request: Request, order_product: Order_products):
@@ -72,6 +83,8 @@ async def add_product_order(order_id: UUID, request: Request, order_product: Ord
         detail=f"Order with id: {order_id} does not exist"
     )
 
+# Update order status information    
+
 @app.patch("/api/orders/{order_id}")
 async def update_order_info(order_update: OrderUpdateRequest, order_id: UUID):
     for order in db_orders:
@@ -84,8 +97,10 @@ async def update_order_info(order_update: OrderUpdateRequest, order_id: UUID):
         detail=f"Order with id: {order_id} does not exist"
     )
 
+# Update order product information, replace product or change quantity
+
 @app.patch("/api/orders/{order_id}/products/{product_id}")
-async def update_order_info(order_id: UUID, product_id: int, request: Request):
+async def update_order_product_info(order_id: UUID, product_id: int, request: Request):
     payload = await request.json()
     for order in db_orders:
         if order.id == order_id:
